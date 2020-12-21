49ebe4853ce1a55a3e954f24e51828a3d9e51c02
alfaz-id <alfazidtech@gmail.com>
2020-12-21 21:51:35 +0700

    clasess

--- classes.dart
+++ classes.dart
@@ -1,11 +1,29 @@
-class person{
+class Person{
+  String name;
+  int age;
+
+  Person(this.name,this.age){
+      this.name = name;
+      this.age = age;
+  }
+
   String getPersonName(){
     return 'Alfaz';
+  }
+
+  void printIdntity(){
+    print('Name :$name'+" Age :"+age.toString());
   }
 }
 
 void main(){
-  var man = person();
+  // create by  oject
+  var man = Person("",0);
   String name = man.getPersonName();
   print(name);
+
+ // create by contructors
+ var detail = Person("Jhon", 30);
+ detail.printIdntity();
+
 }
